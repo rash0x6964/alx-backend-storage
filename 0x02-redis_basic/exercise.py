e0caed794ade929f8f93c2f5df9d6d88c18d0327
rash0x6964 <rash0x6964@gmail.com>
2024-08-14 13:30:51 +0100

    task 1: Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Redis module """
 
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 import redis
 
@@ -13,17 +13,30 @@ class Cache:
 
     def __init__(self):
         """ Constructor of the redis model """
+
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: UnionOfTypes) -> str:
-        """
-        generate a random key (e.g. using uuid),
-         store the input data in Redis using the
-          random key and return the key.
-        :param data:
-        :return:
-        """
+        """ Store the input data in Redis using the random key """
+
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> UnionOfTypes:
+        """ Convert the data back to the desired format """
+
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ Retrieve the data from Redis and decode it as a UTF-8 string. """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """ Retrieve the data from Redis and convert it to an integer. """
+        return self.get(key, int)
